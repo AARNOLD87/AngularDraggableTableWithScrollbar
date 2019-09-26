8bd176decd60b281998e45781eb571656c0eee78
Adolfo Arnold <adolfo.arnold@blexin.com>
2019-09-26 15:53:48 +0200

    init table with bootstrap class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'AnguralDragColumnInScrollableTable';
+  columns: string[];
+
+  constructor() {
+    this.columns = this.loadColumns();
+  }
+
+  private loadColumns(): string[] {
+    const result = [];
+    for (let i = 0; i < 50; i++) {
+      result.push('Column' + i);
+    }
+
+    return result;
+  }
+
 }
